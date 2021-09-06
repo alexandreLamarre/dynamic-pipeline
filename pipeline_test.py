bbd7160f8a7dddbbbcb995672a7c732e7a7ad865
alexandreLamarre <alex7285@gmail.com>
2021-09-06 14:28:17 -0400

    fixed unittests?

--- pipeline_test.py
+++ pipeline_test.py
@@ -6,22 +6,22 @@ import pipeline
 class TestPipeline(unittest.TestCase):
     def test_parseYAML(self):
         #empty YAML
-        res = pipeline.parseYAML("tests/test0.yaml")
+        res = pipeline.parseYAML("tests/test0.YAML")
         assert(res is None)
         #single map YAML
-        res = pipeline.parseYAML("tests/test1.yaml")
+        res = pipeline.parseYAML("tests/test1.YAML")
         assert(type(res) == dict)
         assert(type(res['hello-world']) == bool)
         assert(res['hello-world'] == True)
 
         #single list YAML
-        res = pipeline.parseYAML("tests/test2.yaml")
+        res = pipeline.parseYAML("tests/test2.YAML")
         assert(type(res) == dict)
         assert(type(res['sample-list']) == list)
         assert(len(res['sample-list']) == 3)
         assert(res['sample-list'] == ["one", "two", "three"])
 
-        res = pipeline.parseYAML("tests/test3.yaml")
+        res = pipeline.parseYAML("tests/test3.YAML")
         assert(type(res) == dict)
         assert(type(res['sample-nested-list']['one']) == list and type(res['sample-nested-list']['two']) == list)
         assert(res['hello-world'] == 'octo')
