a59b8405ff95a4ab82bc52d76aec49f95a49f21e
alexandreLamarre <alex7285@gmail.com>
2021-09-06 21:41:52 -0400

    update documentation

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,17 @@ import time
 from enum import Enum
 import yaml
 
+## @brief The config file to load the dynamic Pipeline from
+#
+config = "pipeline.YAML"
+
+## @brief Map of declared global Object instances in `pipeline.YAML`
+#
+INSTANCES = dict()
+## @brief Map of declared global Variables in `pipeline.YAML`
+#
+VARS = dict()
+
 def parseYAML(file ="pipeline.YAML", callback=None):
     '''!
     @brief parse a YAML file
@@ -28,6 +39,9 @@ def parseYAML(file ="pipeline.YAML", callback=None):
 class PipelineTracker:
     '''!
     @brief Tracks pipeline status for a given Pipeline Artifact
+
+    Stored alongside a Pipeline Artifact in Pipeline class' Pipeline::exec
+    @see Pipeline::exec
     '''
     def __init__(self):
         '''
@@ -38,35 +52,61 @@ class PipelineTracker:
         self.malformed = False
     
     def getReturnValue(self):
+        '''!
+        @brief Gets the return value of the associated Artifact stored in this tracker
+        @see Pipeline::exec
+        '''
         return self.return_value
     
     def getExecStatus(self):
+        '''!
+        @brief Gets the exec status of this tracker
+        '''
         return self.exec
     
     def getMalformed(self):
+        '''!
+        @brief Gets whether or not the associated Artifact is malformed
+        '''
         return self.malformed
 
     def setReturnValue(self, value):
+        '''!
+        @brief Sets the return value from the associated Artifact 
+        
+        @param value: the return value of the associated Artifact
+        '''
         self.return_value = value
     
     def setExecStatus(self, exec):
+        '''!
+        @brief Sets the exec status of the associated Artifact of this tracker
+
+        @param exec the exec status of the associated Artifact of this tracker
+        '''
         self.exec = exec
     
     def setMalformed(self, malformed):
+        '''!
+        @brief Sets whether or not associated Artifact is Malformed
+
+        @param malformed : boolean representing whether or not the artifact is malformed
+        '''
         self.malformed = malformed
 
 
 class PipelineStatus(Enum):
-    '''
-    enumerator class for all of dynamic pipeline return statuses
+    '''!
+    @brief enumerator class for all of dynamic pipeline return statuses
     '''
     OK = 0
     WARNING = 1
     CRITICAL = 2
 
     def __repr__(self):
-        '''
-        @rtype str : string representation of Pipeline status
+        '''!
+        @brief String representation of the PipelineStatus
+        @returns str : string representation of Pipeline status
         '''
         if self.value == 0 : return "Successful!"
         if self.value == 1 : return "Executed with warnings."
@@ -75,8 +115,15 @@ class PipelineStatus(Enum):
         return "Placeholder status"
 
     def __str__(self):
+        '''!
+        @brief String Conversion of the Pipeline Status
+        @returns str : string conversion of Pipeline status
         '''
-        @rtype str : string conversion of Pipeline status
+        return repr(self)
+    
+    def out(self):
+        '''!
+        @brief Output some string representation of the PipelineStatus
         '''
         return repr(self)
 
@@ -86,32 +133,64 @@ class Artifact:
     Pipeline
     '''
 
-    def __init__(self, instance = None, background = False, *args):
-        '''
+    def __init__(self, procs = [], background = False, *args):
+        '''!
+        @brief Constructor of a Pipeline Artifact
         
+        @param procs : List of string representations of processes declared in config file
+        @param background : whether or not it is a background process
+        @param args: optional args to be passed to some procs
         '''
         self.status = PipelineTracker()
     
     def execute(self):
-        '''
-        Execute the artifact specified
+        '''!
+        @brief Execute the processes of the Artifact
         '''
         pass
+    
+    def compile():
+        '''!
+        @brief 'Compile' the shell, module, or dynamic python code declared in the
+        config file for this Artifact
+        '''
 
-class ConcurrentArtifact:
+
+    def compilePython(code):
+        '''!
+        @brief Loads dynamic string representing python code into python executable code
+
+        This is the function that is used to load/execute strings `python`/`py`/`python3`/`py3`
+        tags in the pipeline.YAML config file
+        @param code str
+        '''
+    
+    def compileShell(code):
+        '''!
+        @brief Loads dynamic string representation of shell code into python executable code
+        '''
+
+class ConcurrentArtifact(Artifact):
     '''!
     @brief A Concurrent Artifact represents a parallelized - concurrent workload
     to be executed by the Pipeline
     '''
 
-    def __init__(self, artifacts):
+    def __init__(self, procs = [], background = False, *args):
         '''!
-        @brief 
-        
+        @brief  Constructor of a Pipeline Concurrent Artifact
+
+        @param procs : List of string representations of processes declared in config file
+        @param background : whether or not it is a background process
+        @param args: optional args to be passed to some procs
         '''
-        self.status = PipelineTracker()
+        super.__init__(self, procs = [], background = False, *args)
+        
 
     def execute(self):
+        '''!
+        @brief Execute the concurrent processes of the Artifact
+        '''
         pass
 
 
@@ -124,13 +203,13 @@ class Pipeline:
     which can be regular or multi-threaded workloads
     '''
 
-    def __init__(self, verbose = False):
+    def __init__(self):
         '''!
         @brief Pipeline constructor
         '''
         ## @brief Controls whether or not the output of the pipeline is mirrored in stdout
         #
-        self.verbose = verbose
+        self.verbose = False
         ## @brief Start time of the pipeline
         #
         self.startTime = str(time.time())
@@ -144,14 +223,6 @@ class Pipeline:
         #
         self._exec = [] # zipped list of artifacts and their Pipeline Tracker
 
-        ## @brief Declared instances
-        #
-        self.instances = []
-
-
-        ## @brief Map of dynamic variables declared in YAML config file
-        #
-        self.vars = dict()
     
     def write_log(self, msg):
         '''!
@@ -194,7 +265,8 @@ class Pipeline:
         @brief Loads a pipeline to execute from a YAML file called 'pipeline.YAML' by default
         but this can be overriden when you construct the actual Pipeline.
 
-        Returns some nested dictionary/ list combination type
+        @throws yaml.YAMLError : if the .YAML file is malformed
+        @returns dict: dict loaded 
         '''
         
 
@@ -203,12 +275,5 @@ class Pipeline:
         @brief load and form the pipeline after loading the YAML config file
         '''
     
-    def compilePython(code):
-        '''!
-    @brief Loads dynamic string representing python code into python executable code
-
-    This is the function that is used to load/execute strings `python`/`py`/`python3`/`py3`
-    tags in the pipeline.YAML config file
-    @param code str
-    '''
+    
     
